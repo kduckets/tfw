3c287a6225d5414dd443ae7059733cf5b5b6ccf7
Kev <kmditroia@gmail.com>
2021-01-08 19:24:46 -0500

    first

--- pages/index.js
+++ pages/index.js
@@ -4,206 +4,165 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>The Flock Wars</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&family=Inconsolata&display=swap" rel="stylesheet"/>
       </Head>
 
-      <main>
+        <div className="fullHeightCentered">
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         The Flock Wars
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+        <div className="prologue">
+        <p>Prologue</p>
+        <p>The year is 2020. The great thinkers of earth had sensed struggle and unrest brewing for quite some time, 
+          but very few were prepared for what was to come next. Sickness, suffering, and death were just the tip of the iceberg
+           as an already unstable human race was ravaged by plague, cultural , and warfare. 
+           <p>The following is a collection of diary entries, news articles, letters, and other miscellaneous artifacts 
+           from one group of completely insignificant boys who had been forced to stay in their homes and play video games during this 
+           unprecedented year.</p>
+        </p>
+        </div>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      <div className="fullHeight">
+      <p className="narration"><small><i>An excerpt from General Kev’s diary:</i></small></p>
+      <p className="diary"><b>April 20, 2020</b></p>
+      <p className="diary">
+      The unrest among the group has lead to a few seemingly harmless battles to air some petty grievances. Tonight my battle strategy was sound, but the execution was poor. We had built up a huge border defense on the southern region during the Battle of Caso-Johnson, but left an unfortunate gap when I sent a western army, fully armed, up through Canada. 
+      Devistatingly, I ran outta time just as I was hitting the fortify button. That small opening was all General Johnson's army needed to make its move. 
+      Bruised, but not defeated from the bloody Caso-Johnson war, General Johnson sent his men down through the gap and wreaked havoc on the southern region. 
+      The fight continued in Canada but it was not enough to take down the almighty Johnson. With a damaged south and a once cololorful map 
+      now only scattered with red, General Johnson was able to return north and claim victory once again.
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      The battle is over, but the scars remain.</p>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+      <img src='/xo.png' height="200px"></img>
+      </div>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+      <div className="fullHeight">
+      <p className="narration">Following a weekend of minor quarrels, the hints of chaos and savagery seemed to deflate as quickly as they began. Peace talks were underway in an effort
+      to prevent large-scale warfare. 
+      <br/>
+      <br/>
+      Throughout the ceasefire, the politicians continued to bicker from high atop their castle walls, unable 
+      to reach an agreement. 
+      As egos squabbled, the fragile ice beneath the soldiers’ feet began to crack. 
+      Before too long, another armistice had been broken. The calls for peace had been silenced. 
+      </p>
+      <p>Every so often, calls for harmony will nourish the potential for war...</p>
+      </div>
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+      <div className="fullHeight">
+   <p className="news">
+<large><b>G. Johnson Falls Hard</b></large></p>
+<p className="narration"><small>May 3, 2020</small></p>
+<p className="story">
+<small>The ceasefire proved costly for General Johnson’s troops, 
+as the undefeated veteran collapsed to two previously 
+winless armies in yet another bloody Saturday night. 
+General Dowals, new on the scene of war, quickly rose 
+through the ranks after wiping Deutschland clean of the 
+Almighty Johnson’s post-ceasefire army. While Johnson 
+gathers his battered troops to mansplain the follies of hubris, 
+Jr. Lieutenant "the" Caso, humiliated to be the last war-torn nation without a victory, 
+scolds his soldiers and quietly plots his revenge.</small>
+        </p>
+      </div>
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+      <div className="fullHeight">
+      <p className="narration"><small><i>An excerpt from General Kev’s diary:</i></small></p>
+      <p className="diary"><b>May 4, 2020</b></p>
+      <p className="diary">
+      Alternating between peace and war seems to be the new normal. It's unclear how long this will last, however it doesn't
+       seem to be ending any time soon. Most of the other leaders seem reasonable, so perhaps we can come to some sort of agreement. The only one 
+       I'm truly nervous about is the Jr. Lieutenant. He seems too inexperienced to be dealing with this caliber of war. Someone should keep an eye on him.
+      
+      </p>
+      </div>
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+  <div className="fullHeight">
+  <p className="news"><large><b>The Worth of War</b></large></p>
+<p className="narration"><small>May 10, 2020</small></p>
+<p className="story">
+  <small>The eerie silence of the DMZ was eagerly shattered as Ol General Patch spiked his fancy wine-beer, 
+  loudly declaring warfare across Europe. The destruction continued with the Almighty Johnson 
+  demolishing France for the first victory of the evening. In the battle that followed the Green army was swiftly cast to the 
+  southwest corner, relying on carefully executed alliances and manipulative 
+  lawyer-speak just to survive. Also fighting in the south, the piss-colored Yellow army was prematurely 
+  destroyed in both battles, leaving General Daddison with a fading taste of victory and an army 
+  hungry for vengeance. As tensions grew late into the hours of darkness, General Dowals broke 
+  a strong alliance with General Patch and began his valiant march through the north, only to be stopped 
+  devastatingly short at just 67% of the map. The failed climatic offensive left the door open for General Kev and his Red army to 
+  bleed across the north, claiming the final victory of the night from his tiny camp on Randall’s island.
+  <br/>
+  <br/>
+  <i>“In war, whichever side may call itself the victor, there are no winners, but all are losers”</i>, Jr. Lieutenant 'the' 
+  Caso tells himself as he contemplates his failed alliance strategies. The Junior Lieutenant looks over his 
+  blue army, now appearing purple from the blood soaked uniforms, and quietly tells them to go home and “thank 
+  your mothahs”</small></p>
+  </div>
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+  <div className="fullHeight">
+  <p className="news"><large><b>Jr. L. T. Caso Gets Questionable Win</b></large></p>
+<p className="narration"><small>May 17, 2020</small></p>
+<p className="story">
+  <small>Junior Lieutenant ‘the’ Caso claimed his first victory of the war last night, although rumors of chemical 
+    warfare could prove diminishing to the win. “I felt loopy, I certainly wasn’t myself,” General Rad stated to 
+    reporters when asked why he didn’t use his cards at a crucial battle moment. “I guess it coulda been the anchovies,” 
+    he added, apprehensively. In response to the allegations, Lieutenant the Caso began to describe an awkward dream he 
+    had about the yellow army’s commander, followed by some patronizing advice to the seasoned General. The topics of 
+    chemical warfare and anchovies were suspiciously avoided.
+  <br/>
+  <br/>
+ The only other leader willing to comment on the developing 
+    controversy was General Stevie the B, simply stating, “fucking pussies”. 
+    The General also requested the press to remind its readers, “the B stands for Bossin”.</small></p>
+  </div>
 
-        .title,
-        .description {
-          text-align: center;
-        }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+<div className="fullHeight">
+<p className="narration">June 7, 2020</p>
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+<p className="story"><large><b>The Many Colors of War</b></large></p>
+<p>Chaos and crimes of passion overshadowed any hope for actual strategy, as verbal conflict blended with 
+senseless violence to fuel the cycle of incessant devastation.</p> 
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
+...
 
-          max-width: 800px;
-          margin-top: 3rem;
-        }
+<p className="story"><small>The soft echoes of peace were quickly silenced as Junior Lieutenant 
+'the' Caso dropped his curry dinner, allegedly distracted by the sirenesque 
+sounds of General Dowals’ mistress. Filled with shame and regret, the Lieutenant 
+cracked a can of watery tuna and heeded the calls of war to fill the painful void. 
+The 'drop heard round the world' proved unfortunate for the red army, who shared a border 
+with the humiliated Lieutenant looking to unleash his anger on a nearest neighbor. A battle of 
+passion broke out, resulting in a depleted red army with no option but to beg for an alliance with 
+General Dowals in the north. Desperate for companionship in wartime, the General accepted the offer 
+and the orange-red army marched to destroy any hopeless soldiers in its path. Unable to band together 
+against the Goliath, the nations resorted to blame and slander as they fell pitifully to orange, all 
+while a tuna-breathed Lieutenant continued to stir the pot of conflict from the grave. 
+<br/>
+<br/>
+Perhaps out of both breathe and energy, the yelling calmed in Africa as the nations all turned against Orange, 
+leaving an opening for a win-starved Purple army to deploy a fresh strategy. The decision to not camp in a 
+single country proved fruitful for Ol General Patch as he was able to build and bleed through the war-torn 
+nations for a much needed Purple victory.
+<br/>
+<br/>
+The battle turned to the grounds of the Roman Empire, where the generals, 
+seemingly unable to learn from the past, began to repeat history (again). The 
+coastal quarrels quickly boiled over, leaving room for the armies inland to grow and plot. 
+With a valiant effort to break the infinite loops of the days of old, the Red army marched with 
+hope and passion, only to fall by the limits of their own weaponry. With the privilege of superior 
+funding, artillery, and technology, General CJ crushed the hope of the passionate Red army (and 
+perhaps the world) allowing a blood-colored history to once again, repeat itself.</small></p>
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+</div>
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
