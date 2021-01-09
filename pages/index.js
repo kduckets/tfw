b9ca24a83a7feb5e8fe36715e295fb59f48fe440
Kev <kmditroia@gmail.com>
2021-01-08 19:38:57 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
         <title>The Flock Wars</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&family=Inconsolata&display=swap" rel="stylesheet"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </Head>
 
         <div className="fullHeightCentered">
