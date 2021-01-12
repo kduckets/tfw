86be206d47b842e5d769377c16bdaedff438c5f6
kduckets <kmditroia@gmail.com>
2021-01-12 08:11:01 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -472,7 +472,7 @@ END MEMO
 <div className="fullHeight">
   <p className="narration">
     The summer of 2020 ended more peacefully than it began for the dudes, while the world entered an autumn with slight hints of deceptive 
-    normalcy. A peaceful fall would soon give rise to a warring winter with ubiquitous tension and conflict threading through every fabric of humanity.
+    normalcy. A peaceful fall would soon give rise to a warring winter with tension, conflict, and death threading through every fabric of humanity.
      They say it tends to be darkest before the 
     dawn...
   </p>
