c049e19ffa3bbb3c14b2080f90159466ef80a824
Kev <kmditroia@gmail.com>
2021-01-13 17:06:05 -0500

    yarn img

--- pages/index.js
+++ pages/index.js
@@ -494,7 +494,7 @@ END MEMO
     now too familiar tension, conflict, and demise threading through every fabric of humanity.
     They say it tends to be darkest before the dawn...
   </p>
-  <img src="/yarn.png" height="200px" className="center"></img>
+  <img src="/yarn.png" className="center"></img>
 </div>
 
 <div className="fullHeight">
