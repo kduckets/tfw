73e04ff7c7c0cbb32162b409f583e7db16daa8b2
Kev <kmditroia@gmail.com>
2021-01-13 16:07:44 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ scolds his soldiers and quietly plots his revenge.
 
       <div className="fullHeight">
       <p className="narration">
-      While the turmoil continued to simmer, politicians bickered from high atop their castle walls. As egos squabbled, 
+      As the turmoil continued to simmer, politicians ruthlessly bickered from high atop their castle walls. While egos squabbled, 
       unable to reach agreement, the fragile ice beneath the soldiers’ feet began to crack. 
       Before long, another armistice had been broken. The calls for peace had been silenced. Even the dick jokes were going limp.
       </p>
