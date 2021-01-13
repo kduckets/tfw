c98b0567b1f693ab225e06044d2dba4495a545e2
Kev <kmditroia@gmail.com>
2021-01-13 10:58:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
       The unrest amongst the group has lead to a few seemingly harmless battles to air some petty grievances. 
       Tonight my battle strategy was sound, but the execution was poor. We had built up a huge border defense on 
       the southern region during the Battle of Caso-Johnson, but left an unfortunate gap after sending a western army up 
-      through Canada and ran outta time just as I was hitting the fortify button. That small opening was all the Johnson needed for penetration. 
+      through Canada. I ran outta time just as I was hitting the fortify button - an infuriating mistake to say the least. That small opening was all the Johnson needed for penetration. 
       Bruised, but not defeated from the bloody Caso-Johnson debacle, the Johnson perforated the gap and wreaked havoc on the southern region. 
       The fight continued in Canada but it was not enough to take down the almighty Johnson. With a damaged south and a once cololorful map 
       now only scattered with red, General Johnson was able to pull out, scatter soldiers up north, and claim victory once again.
