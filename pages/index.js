56c7dbcce5cad81b29c26c13e515e54cb58c4e53
kduckets <kmditroia@gmail.com>
2021-01-15 14:09:59 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -337,7 +337,7 @@ The Junior Lieutenant turned and scampered out, suddenly aware that he had, in f
 
 <div className="fullHeight">
   <p className="narration">
-  With no end in sight, some took escape to the wilderness
+  With no end in sight, many took escape to the wilderness
    to seek solace from the isolation and communal devastation. Nature is left once all else is distilled.
   </p>
   <img src="/mountains.png" className="center"></img>
