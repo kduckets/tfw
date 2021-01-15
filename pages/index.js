8e1e7f28137b6ac3ba908e1dc256841a21ac5c86
kduckets <kmditroia@gmail.com>
2021-01-14 21:12:18 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
       The fight continued in Canada but it was not enough to take down the almighty Johnson. With a damaged south and a once cololorful map 
       now only scattered with red, General Johnson was able to pull out of the south, scatter soldiers up north, and claim victory once again.
 
-      Johnson's patient strategy has proven quite difficult to defeat. The battle is over, but the scars remain. 
+      Johnson's patient strategy has proven quite difficult to defeat. Tonight's battle is over, but the scars remain. 
     
       </p>
      
