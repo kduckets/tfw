89bd4d89b9be5d02d71e87c21ea7afc018ff1b30
kduckets <kmditroia@gmail.com>
2021-01-15 06:37:07 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -65,8 +65,7 @@ export default function Home() {
 
       <div className="fullHeight">
       <p className="diary">
-      <h4>Private Property of General Kev  -  Keep Out</h4>
-        <h1>April 20, 2020</h1>
+        <h2>April 20, 2020</h2>
    
       The unrest amongst the dudes has lead to a few seemingly harmless battles to air some petty grievances. 
       Tonight I ran outta time just as I was hitting the fortify button - an infuriating mistake to say the least. 
